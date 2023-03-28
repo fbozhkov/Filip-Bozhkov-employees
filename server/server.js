9056039386e8f31bc1fb4d88baec8925fe185ba1
thecarguy97 <filip.boshkov@gmail.com>
2023-03-28 13:45:41 +0300

    init

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express, { json } from "express";
+import cors from "cors";
+import multer from "multer";
+import { readFileSync, unlinkSync } from "fs";
+import path from "path";
+import { processCsv } from "./processCsv";
+
+const upload = multer({ dest: "uploads/" });
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(json());
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    const data = readFileSync(req.file.path, "utf8");
+    unlinkSync(req.file.path);
+
+    const result = processCsv(data);
+    res.json(result);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
